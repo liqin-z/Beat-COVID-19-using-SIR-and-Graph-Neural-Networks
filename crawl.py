4cc9fe10a48a29b1e88cb25d58e31e03287d33a5
graveszhang <graves_zhang@sjtu.edu.cn>
2020-01-27 16:01:25 +0800

    generate data.json

--- crawl.py
+++ crawl.py
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 import json
-=======
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
-# 引用 json 库用于解析 json 对象
-import json
-# 使用 requests 库
->>>>>>> 6fba61b449b98d92bad8244d3401e72cc5158ffe
 import requests
 response = requests.get('http://cxz.moe:1314/2019-nCoV/areas')
 versionInfo = response.text
 # versionInfo 是接口返回的 json 格式数据
-<<<<<<< HEAD
 
 versionInfoPython = json.loads(versionInfo)
 # json.loads 将已编码的 JSON 字符串解码为 Python 对象
@@ -22,20 +13,3 @@ dataList = versionInfoPython.get('data')
 
 with open('data.json', 'w') as outfile:
     json.dump(dataList, outfile, ensure_ascii=False)
-=======
-# json.loads 将已编码的 JSON 字符串解码为 Python 对象
-versionInfoPython = json.loads(versionInfo)
-# print (versionInfoPython)
-dataList = versionInfoPython.get('data')
-#print(dataList)
-
-with open('data.json', 'w') as outfile:
-    json.dump(dataList, outfile,ensure_ascii=False)
-
-import pandas as pd
-import numpy as np
-
-#data=pd.read_json(dataList)
-
-
->>>>>>> 6fba61b449b98d92bad8244d3401e72cc5158ffe
