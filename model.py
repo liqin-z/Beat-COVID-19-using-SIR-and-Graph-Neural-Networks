031dd0a53e2b276615e86660313f13ef85795daa
graveszhang <graves_zhang@sjtu.edu.cn>
2020-01-28 11:53:44 +0800

    map2 updated

--- model.py
+++ model.py
@@ -16,26 +16,45 @@ with open(filename) as f:
 
 from snapshot_pyppeteer import snapshot
 from pyecharts.charts import Geo
+from pyecharts.charts import Map
 from pyecharts import options as opts
 from pyecharts.render import make_snapshot
 from pyecharts.globals import ChartType, SymbolType
 
-def geo_heatmap() -> Geo:
+def geo_getmap() -> Geo:
     c = (
         Geo()
         .add_schema(maptype="china")
         .add(
             "Confirmed Cases",
             allcases,
-            type_=ChartType.HEATMAP,
+            # type_=ChartType.HEATMAP,
         )
         .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
         .set_global_opts(
-            visualmap_opts=opts.VisualMapOpts(),
+            visualmap_opts=opts.VisualMapOpts(max_=100, is_piecewise=True),
             title_opts=opts.TitleOpts(title="2019-nCoV Heat Map(China)"),
         )
     )
     make_snapshot(snapshot, c.render(), "geo.png")
 
+def map_getmap() -> Map:
+    c = (
+        Map()
+        .add(
+            "Confirmed Cases",
+            allcases,
+            "china"
+            # type_=ChartType.HEATMAP,
+        )
+        .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+        .set_global_opts(
+            visualmap_opts=opts.VisualMapOpts(max_=100, is_piecewise=True),
+            title_opts=opts.TitleOpts(title="2019-nCoV Heat Map(China)"),
+        )
+    )
+    make_snapshot(snapshot, c.render(), "map.png")
+
 if __name__ == '__main__':
-    geo_heatmap()
+    # geo_getmap()
+    map_getmap()
